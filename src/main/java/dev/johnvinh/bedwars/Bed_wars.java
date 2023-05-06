09cca5c15e83fb84957eb1c746699b6e7de56348
John Vinh <john.michael.vinh2@gmail.com>
2023-05-05 20:11:50 -0400

    Initial commit

---
+++ src/main/java/dev/johnvinh/bedwars/Bed_wars.java
@@ -0,0 +1,17 @@
+package dev.johnvinh.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bed_wars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
